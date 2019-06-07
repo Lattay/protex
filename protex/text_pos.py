72355cebf53eea33a62c94a3e7b2f83c58c554a5
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2019-06-07 20:22:12 +0200

    Move TextPos related thing to a dedicated submodule

---
+++ protex/text_pos.py
@@ -0,0 +1,130 @@
+class TextPos:
+    def __init__(self, col, line):
+        self.col = col
+        self.line = line
+
+    def as_dict(self):
+        return {
+            'col': self.col,
+            'line': self.line
+        }
+
+    def __repr__(self):
+        return self.__class__.__name__ + '({}, {})'.format(self.col, self.line)
+
+    def __str__(self):
+        return 'L{}C{}'.format(self.line, self.col)
+
+    def __add__(self, num):
+        if isinstance(num, int):
+            return TextPos(self.col + num, self.line)
+        elif isinstance(num, TextDeltaPos):
+            return TextPos(self.col if num.line == 0 else num.col,
+                           self.line + num.line)
+        else:
+            raise ValueError('Cannot add TextPos with {}'.format(num))
+
+    def __radd__(self, num):
+        return self + num
+
+    def __sub__(self, other):
+        assert self >= other
+        if self == other:
+            return TextDeltaPos(0, 0)
+        else:
+            return TextDeltaPos(
+                self.col - other.col if self.line == other.line else other.col,
+                self.line - other.line
+            )
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __gt__(self, other):
+        return self.line > other.line or (self.line == other.line
+                                          and self.col > other.col)
+
+    def __ge__(self, other):
+        return self.line > other.line or (self.line == other.line
+                                          and self.col >= other.col)
+
+    def __lt__(self, other):
+        return not (self >= other)
+
+    def __le__(self, other):
+        return not (self > other)
+
+    def __eq__(self, other):
+        return self.line == other.line and self.col == other.col
+
+
+class TextDeltaPos(TextPos):
+    @classmethod
+    def from_src(self, src):
+        lines = src.split('\n')
+        if lines:
+            return TextDeltaPos(len(lines[-1]), len(lines))
+        else:
+            return TextDeltaPos(0, 0)
+
+    def __str__(self):
+        if self.line == 0:
+            return 'c+{}'.format(self.col)
+        else:
+            return 'l+{}C{}'.format(self.line, self.col)
+
+
+class PosMap:
+    pass
+
+
+class ContiguousPosMap(PosMap):
+    def __init__(self, src_start, src_end, final_start, final_end):
+        self.src_start = src_start
+        self.src_end = src_end
+        self.final_start = final_start
+        self.final_end = final_end
+
+
+class RootPosMap(PosMap):
+    def __init__(self, filename, maps):
+        self.filename = filename
+        self.src_start = 0
+        self.maps = self.sort(maps)
+
+    def sort(self, maps):
+        return sorted(maps, key=lambda it: it.src_start)
+
+    def _for_all(self):
+        root_stack = [self]
+        while root_stack:
+            root = root_stack.pop()
+            yield ('file', root.filename)
+            for map in root.maps:
+                if isinstance(map, RootPosMap):
+                    root_stack.append(map)
+                else:
+                    yield ('map', map)
+
+    def as_text(self):
+        lines = []
+        for t, obj in self._for_all():
+            if t == 'file':
+                lines.append('[{}]'.format(obj))
+            else:
+                lines.append('{}-{}={}-{}'.format(*obj))
+        return '\n'.join(lines)
+
+    def as_dict(self):
+        d = {}
+        fname = None
+        for t, obj in self._for_all():
+            if t == 'file':
+                fname = obj
+                d[fname] = []
+            else:
+                d[fname].append({
+                    'src': (obj[0].as_dict(), obj[1].as_dict()),
+                    'dest': (obj[2].as_dict(), obj[3].as_dict())
+                })
+        return d
